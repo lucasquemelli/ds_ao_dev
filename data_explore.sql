9136f12deeccf25360755e6367b533246da9580d
Lucas Quemelli <lucasquemelli@gmail.com>
2022-10-19 11:08:54 -0300

    inserting SQL files

--- data_explore.sql
+++ data_explore.sql
@@ -27,3 +27,16 @@ FROM products
 WHERE LOWER(product_category_name) IN ('perfumaria', 'artes')
 AND product_weight_g >= 1000
 ORDER BY 6 DESC; 
+
+SELECT 
+	product_category_name,
+	CASE 
+		WHEN product_height_cm < 10 THEN 'small'
+		WHEN product_height_cm BETWEEN 10 AND 15 THEN 'medium'
+		ELSE 'large'
+	END product_size,
+	COUNT(DISTINCT product_id) AS distinct_products
+FROM products
+WHERE product_category_name IS NOT NULL
+GROUP BY 1,2
+ORDER BY 3 DESC;
