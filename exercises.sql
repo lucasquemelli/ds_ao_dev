7e2a1ab409d062bc661390b1477ef0d05f3d1dc7
Lucas Quemelli <lucasquemelli@gmail.com>
2022-10-19 14:36:47 -0300

    exercises update

--- exercises.sql
+++ exercises.sql
@@ -1,4 +1,4 @@
--- 1. Quantos pedidos foram feitos para cada tipo de pagamento?
+-- 01. Quantos pedidos foram feitos para cada tipo de pagamento?
 
 SELECT 
 	payment_type,
@@ -7,14 +7,14 @@ FROM order_payments
 GROUP BY 1
 ORDER BY 2 DESC;
 
--- 2. Qual o número máximo e mínimo de parcelas nos pagamentos?
+-- 02. Qual o número máximo e mínimo de parcelas nos pagamentos?
 
 SELECT 
 	MAX(payment_installments) max_division,
 	MIN(payment_installments) min_division
 FROM order_payments;
 
--- 3. Quais são os top 10 pedidos com os maiores valores?
+-- 03. Quais são os top 10 pedidos com os maiores valores?
 
 SELECT 
 	order_id,
@@ -23,3 +23,14 @@ FROM order_payments
 ORDER BY 2 DESC
 LIMIT 10;
 
+-- 04. Quais são os últimos 10 pedidos com os menores valores?
+
+SELECT
+	op.order_id,
+	order_purchase_timestamp,
+	op.payment_value 
+FROM order_payments op 
+INNER JOIN orders o ON op.order_id = o.order_id 
+WHERE op.payment_value > 0
+ORDER BY 3 ASC, 2 DESC
+LIMIT 10;
