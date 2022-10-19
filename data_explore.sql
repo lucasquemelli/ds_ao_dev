929f18aa63fc9ed1e3f70d1456fbfb665008f6f7
Lucas Quemelli <lucasquemelli@gmail.com>
2022-10-19 10:56:54 -0300

    Add files via upload

---
+++ data_explore.sql
@@ -0,0 +1,29 @@
+SELECT
+	customer_state,
+	COUNT(DISTINCT customer_id) AS distinct_customers,
+	COUNT(customer_unique_id) AS distinct_auto_customers
+FROM customer 
+GROUP BY 1
+ORDER BY 2 DESC;
+
+SELECT
+	product_id,
+	product_category_name,
+	product_height_cm,
+	product_length_cm,
+	product_width_cm,
+	product_weight_g,
+	(product_height_cm * product_length_cm * product_width_cm) AS volume_cm3,
+	CASE 
+		WHEN LOWER(product_category_name) = 'perfumaria' THEN 'store'
+		ELSE 'website'
+	END AS source,
+	CASE 
+		WHEN product_height_cm < 10 THEN 'small'
+		WHEN product_height_cm BETWEEN 10 AND 15 THEN 'medium'
+		ELSE 'large'
+	END product_size
+FROM products
+WHERE LOWER(product_category_name) IN ('perfumaria', 'artes')
+AND product_weight_g >= 1000
+ORDER BY 6 DESC; 
---
+++ tables_test.sql
@@ -0,0 +1,15 @@
+SELECT * FROM customer c;
+
+SELECT * FROM order_items oi; 
+
+SELECT * FROM order_payments op; 
+
+SELECT * FROM order_reviews or2; 
+
+SELECT * FROM orders o;
+
+SELECT * FROM geolocation g;
+
+SELECT * FROM product_category pc;
+
+SELECT * FROM products p; 
